fdcb3c17dc1798579a12984002e986bb907b380d
LandonP172 <141693593+LandonP172@users.noreply.github.com>
2024-01-25 14:37:43 -0700

    start to landing page and header

--- portfolio/src/components/Header.jsx
+++ portfolio/src/components/Header.jsx
@@ -1,9 +1,44 @@
 import React from 'react'
 
 function Header() {
+    const menuList= [
+        {
+            id:1,
+            title:'Home'
+        },
+        {
+            id:1,
+            title:'About'
+        },
+        {
+            id:1,
+            title:'Service'
+        },
+        {
+            id:1,
+            title:'Portfolio'
+        },
+        {
+            id:1,
+            title:'Contact'
+        }
+    ]
   return (
-    <div>Header</div>
-  )
+    <div>
+    <div>
+        <h2 className='text-[25px] font-bold text-white'>Landon
+            <span className='text-red-500'> Peterson</span>
+        </h2>
+    </div>
+    <div>
+        {menuList.map((item) => (
+            <div>
+                <h2 className='text-white'>{item.title}</h2>
+            </div>
+        ))}
+    </div>
+</div>
+);
 }
 
 export default Header
