49278b5292befd9150737688eb2aac8141d5d7e0
LandonP172 <141693593+LandonP172@users.noreply.github.com>
2024-01-25 12:37:16 -0700

    restarted project and am sad

--- portfolio/src/components/Header.jsx
+++ portfolio/src/components/Header.jsx
@@ -1,20 +1,9 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react'
 
 function Header() {
   return (
-    <header>
-      <h1>Developer's Name</h1>
-      <nav>
-        <ul>
-          <li><Link to="/about">About Me</Link></li>
-          <li><Link to="/portfolio">Portfolio</Link></li>
-          <li><Link to="/contact">Contact</Link></li>
-          <li><Link to="/resume">Resume</Link></li>
-        </ul>
-      </nav>
-    </header>
-  );
+    <div>Header</div>
+  )
 }
 
-export default Header;
+export default Header
