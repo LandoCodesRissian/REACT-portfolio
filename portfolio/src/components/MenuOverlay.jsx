5e729daf0ea59da1c1aaa3e06de6f209157dd151
LandonP172 <141693593+LandonP172@users.noreply.github.com>
2024-01-25 19:43:09 -0700

    some styling for MenuOverlay

--- portfolio/src/components/MenuOverlay.jsx
+++ portfolio/src/components/MenuOverlay.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 
 function MenuOverlay({menuList}) {
   return (
-    <div>
+    <div className='absolute left-0 min-full backdrop-blur-lg w-full mt-7'>
         {menuList.map((item, index)=> (
-            <h1 key={index}>{item.title}</h1>
+            <h1 key={index} className='text-white text-[24px] mb-6 justify-center mt-10
+            hover:scale-110 transition-all ease-in-out duration-300 cursor-pointer'>{item.title}</h1>
         ))}
     </div>
   )
